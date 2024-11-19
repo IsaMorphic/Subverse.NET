08f27dce03a32ae0d990f8febad73037e35e5f29
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-11-19 13:12:15 -0600

    meow

--- Subverse.Calculator/Program.cs
+++ Subverse.Calculator/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Subverse.Calculator;
 using System.Numerics;
+using System.Security.Cryptography;
 
-int N, K;
 using StreamWriter writer = File.CreateText("results.csv");
 writer.AutoFlush = true;
 writer.WriteLine("N,K,P");
-for (N = 2; N <= 8; N++)
+Parallel.For(2, 9, N =>
 {
     ulong ident = BitMatrix.Identity(N).Bits;
     IEnumerable<ulong> PermuteBits(int M, int K)
@@ -30,18 +30,25 @@ for (N = 2; N <= 8; N++)
         }
     }
 
-    for (K = 1; K <= 12; K++)
+    Parallel.For(1, 13, K =>
     {
         long totalPopCount = 0, totalPermCount = 0;
-        Parallel.ForEach(PermuteBits(N * N, K),
-            (v, ct) =>
-            {
-                BitMatrix A = new(N, v);
-                BitMatrix R_n = BitMatrix.Warshall(A);
+        foreach (ulong v in PermuteBits(N * N, K)
+            .Where(x => N < 6 || K < 8 || RandomNumberGenerator.GetInt32(1 << (K + 9)) == 0))
+        {
+            BitMatrix A = new(N, v);
+            BitMatrix R_n = BitMatrix.Warshall(A);
 
-                Interlocked.Add(ref totalPopCount, BitOperations.PopCount(R_n.Bits & ~ident));
-                Interlocked.Increment(ref totalPermCount);
-            });
-        writer.WriteLine($"{N},{K},{totalPopCount / (double)(totalPermCount * N * (N - 1))}");
-    }
-}
+            totalPopCount += BitOperations.PopCount(R_n.Bits & ~ident);
+            ++totalPermCount;
+        }
+
+        string line = $"{N},{K},{totalPopCount / (double)(totalPermCount * N * (N - 1))}";
+        Console.WriteLine(line);
+
+        lock (writer)
+        {
+            writer.WriteLine(line);
+        }
+    });
+});
