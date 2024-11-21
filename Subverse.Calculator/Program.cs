3e6a61da952b680c084c8a676572907845b011f5
Isabelle Santin <isabelle@chosenfewsoftware.com>
2024-11-21 14:11:28 -0600

    meow

--- Subverse.Calculator/Program.cs
+++ Subverse.Calculator/Program.cs
@@ -47,7 +47,7 @@ Parallel.For(MIN_N, MAX_N + 1, N =>
         Random rng = new();
         long totalPopCount = 0, totalPermCount = 0;
         IEnumerable<ulong> V = N < 6 || K < 8 ? PermuteBits(N * N, K) 
-            : PermuteBits(N * N, K).PickN(rng, 1 << 16);
+            : PermuteBits(N * N, K).PickN(rng, 1 << 14);
 
         Parallel.ForEach(V, v =>
         {
